4ab81aa6c01bfddc3aa740f7d5f4aa3b4386ad14
Hugo Eduardo Fidelis da silva <01360589228@ciccr-am.interno>
2020-02-03 10:26:50 -0400

    Segundo Commit

---
+++ src/components/chats/ChatHeading.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import FAVideo from 'react-icons/lib/fa/video-camera'
+import FAUserPlus from 'react-icons/lib/fa/user-plus'
+import MdEllipsisMenu from 'react-icons/lib/md/keyboard-control'
+
+export default function({name, numberOfUsers}) {
+
+	console.log(name)
+	
+	return (
+		<div className="chat-header">
+			<div className="user-info">
+				<div className="user-name">{name.split(' & ')[0]}</div>
+				<div className="status">
+					<div className="indicator"></div>
+					<span>{numberOfUsers ? numberOfUsers : null}</span>
+				</div>
+			</div>
+
+		</div>
+	);
+	
+}
